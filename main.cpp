1d865f49e042caaea4248de4c04c471b47ba72e5
Jorikas <joris.valtaris@gmail.com>
2025-05-13 14:17:25 +0200

    Update main.cpp
    changed the main function to include commands meant for testing the different functions from movement files

--- main.cpp
+++ main.cpp
@@ -1,20 +1,44 @@
-//#include "test.h"   //Examples
-#include "MotorDriver.h"
-
-// #include "pico/stdlib.h"
-// #include <stdio.h>
+// #include "test.h"   //Examples
+// #include "MotorDriver.h"
+#include "MotorClass.h"
+#include <pico/time.h>
 
 int main(void)
 {
-    //test();
-    // stdio_init_all();
-    // printf("WS2812 Smoke Test, using pin ");
+    uint current_time = to_ms_since_boot(get_absolute_time());
+    Motor a;
 
     while(1){
+        // while (current_time <= 2000) {
+        //     a.forward_move(100);
+        //     current_time = to_ms_since_boot(get_absolute_time());
+        // }
+        // a.stop();
 
-        Motor_test();
+        // while (current_time <= 4000) {
+        //     a.backward_move(100);
+        //     current_time = to_ms_since_boot(get_absolute_time());
+        // }
+        // a.stop();
 
+        // while (current_time <= 6000) {
+        //     a.left_lateral(100);
+        //     current_time = to_ms_since_boot(get_absolute_time());
+        // }
+        // a.stop();
+
+        // while (current_time <= 8000) {
+        //     a.right_lateral(100);
+        //     current_time = to_ms_since_boot(get_absolute_time());
+        // }
+        // a.stop();
+
+        a.turn_left(100);
         sleep_ms(1000);
+        a.turn_right(100);
+        sleep_ms(1000);
+        a.turn_180(100);
+        sleep_ms(15000);
     }
     
     return 0;
