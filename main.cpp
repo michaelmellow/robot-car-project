162415f6562ec477c5f73d82e94024807cfa1a52
Jorikas <joris.valtaris@gmail.com>
2025-05-13 14:42:55 +0200

    Update main.cpp
    update to main function test for motors

--- main.cpp
+++ main.cpp
@@ -9,29 +9,29 @@ int main(void)
     Motor a;
 
     while(1){
-        // while (current_time <= 2000) {
-        //     a.forward_move(100);
-        //     current_time = to_ms_since_boot(get_absolute_time());
-        // }
-        // a.stop();
+        while (current_time <= 2000) {
+            a.forward_move(100);
+            current_time = to_ms_since_boot(get_absolute_time());
+        }
+        a.stop();
 
-        // while (current_time <= 4000) {
-        //     a.backward_move(100);
-        //     current_time = to_ms_since_boot(get_absolute_time());
-        // }
-        // a.stop();
+        while (current_time <= 4000) {
+            a.backward_move(100);
+            current_time = to_ms_since_boot(get_absolute_time());
+        }
+        a.stop();
 
-        // while (current_time <= 6000) {
-        //     a.left_lateral(100);
-        //     current_time = to_ms_since_boot(get_absolute_time());
-        // }
-        // a.stop();
+        while (current_time <= 6000) {
+            a.left_lateral(100);
+            current_time = to_ms_since_boot(get_absolute_time());
+        }
+        a.stop();
 
-        // while (current_time <= 8000) {
-        //     a.right_lateral(100);
-        //     current_time = to_ms_since_boot(get_absolute_time());
-        // }
-        // a.stop();
+        while (current_time <= 8000) {
+            a.right_lateral(100);
+            current_time = to_ms_since_boot(get_absolute_time());
+        }
+        a.stop();
 
         a.turn_left(100);
         sleep_ms(1000);
